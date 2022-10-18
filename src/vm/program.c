0ca7cfd9f30d42b3ca9489205f23dfba5d3f5d0d
Bill Zissimopoulos <billziss@navimatics.com>
2022-10-18 15:52:55 +0100

    convert tabs to spaces

--- src/vm/program.c
+++ src/vm/program.c
@@ -16,38 +16,38 @@
 
 static int vmrun(int argc, char **argv)
 {
-	vm_result_t result;
-	vm_config_t config;
-	vm_t *instance = 0;
+    vm_result_t result;
+    vm_config_t config;
+    vm_t *instance = 0;
 
-	memset(&config, 0, sizeof config);
-	config.cpu_count = 1;
-	config.memory_size = 4096;
+    memset(&config, 0, sizeof config);
+    config.cpu_count = 1;
+    config.memory_size = 4096;
 
-	result = vm_create(&config, &instance);
-	if (!vm_result_check(result))
-		goto exit;
+    result = vm_create(&config, &instance);
+    if (!vm_result_check(result))
+        goto exit;
 
-	vm_set_debug_log(instance, (unsigned)-1);
+    vm_set_debug_log(instance, (unsigned)-1);
 
-	result = vm_start_dispatcher(instance);
-	if (!vm_result_check(result))
-		goto exit;
+    result = vm_start_dispatcher(instance);
+    if (!vm_result_check(result))
+        goto exit;
 
-	vm_wait_dispatcher(instance);
+    vm_wait_dispatcher(instance);
 
-	vm_stop_dispatcher(instance);
+    vm_stop_dispatcher(instance);
 
 exit:
-	if (0 != instance)
-		vm_delete(instance);
+    if (0 != instance)
+        vm_delete(instance);
 
-	return vm_result_check(result) ? 0 : 1;
+    return vm_result_check(result) ? 0 : 1;
 }
 
 int main(int argc, char **argv)
 {
-	return vmrun(argc, argv);
+    return vmrun(argc, argv);
 }
 
 EXEMAIN;
