cd8b1b54611d39f212db04a9090315f68aa7c480
Bill Zissimopoulos <billziss@navimatics.com>
2022-11-18 16:42:41 +0000

    tst: vm-tests

---
+++ tst/vm-tests/vm-tests.c
@@ -0,0 +1,21 @@
+/**
+ * @file vm-tests.c
+ *
+ * @copyright 2022 Bill Zissimopoulos
+ */
+/*
+ * This file is part of VirtualMetal.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License version 3 as published by the Free
+ * Software Foundation.
+ */
+
+#include <vm/internal.h>
+#include <tlib/testsuite.h>
+
+int main(int argc, char **argv)
+{
+    tlib_run_tests(argc, argv);
+    return 0;
+}
